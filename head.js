f9ee91d4d0ebba67191cc9fa72bd79dfdf1b6a69
vagrant <wong_michael@rogers.com>
2021-10-21 04:18:49 +0000

    Using assertEqual with first element of an array

---
+++ head.js
@@ -0,0 +1,18 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const head = function(list) {
+  if (list) {
+    return list[0];
+  } else {
+    return undefined;
+  }
+};
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
