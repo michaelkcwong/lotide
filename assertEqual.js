4004e9b9936dbd1dae43d360bc71e4f625f2c83f
vagrant <wong_michael@rogers.com>
2021-10-21 03:37:02 +0000

    Improved AssertEqual

--- assertEqual.js
+++ assertEqual.js
@@ -1,11 +1,26 @@
+/* //Original (Used concatenate values)
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("🛑🛑🛑 Assertion Passed: " + [actual] + " === " + [expected]);
+    console.log("✅✅✅ Assertion Passed: " + [actual] + " === " + [expected]);
   } else if (actual !== expected) {
-    console.log("✅✅✅ Assertion Failed: " + [actual] + " !== " + [expected]);
+    console.log("🛑🛑🛑 Assertion Failed: " + [actual] + " !== " + [expected]);
   }
 };
 
 // TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+*/
+
+//Improved assertEqual (Used template literals)
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
