3d10d53b15e9e594824d9942a90c71d9fe2229bd
vagrant <wong_michael@rogers.com>
2021-10-23 21:01:29 +0000

    Added assertArraysEqual

--- eqArrays.js
+++ eqArrays.js
@@ -9,13 +9,15 @@ const assertEqual = function(actual, expected) {
 const eqArrays = function(arr1, arr2) {
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
-      return false;
+      return false
     }
   }
   return true;
 };
 
+
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 assertEqual(eqArrays([2, 1, 3], [1, 2, 3]), true);
 assertEqual(eqArrays([2, 1, 3], [1, 2, 3]), false);
 assertEqual(eqArrays(["1", "2", 3], [1, 2, 3]), false);
+assertEqual(eqArrays([1, 0, 2], [5, 3, 1]), true);
