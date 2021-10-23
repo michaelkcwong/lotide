7b7385f67e4bceba9d0fe0080942f4321fbab617
vagrant <wong_michael@rogers.com>
2021-10-23 00:55:35 +0000

    eqArray works

--- eqArrays.js
+++ eqArrays.js
@@ -6,3 +6,16 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+const eqArrays = function(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([2, 1, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([2, 1, 3], [1, 2, 3]), false);
+assertEqual(eqArrays(["1", "2", 3], [1, 2, 3]), false);
