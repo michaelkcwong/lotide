9c80fc9f60a4d6c8f081ac7a18c53ab5135e6107
vagrant <wong_michael@rogers.com>
2021-10-24 03:58:02 +0000

    new flatten.js file

--- without.js
+++ without.js
@@ -37,3 +37,4 @@ assertArraysEqual(words, ["hello", "world", "lighthouse"]);
 
 console.log(without([1, 2, 3], [1]));
 console.log(without(["1", "2", "3"], [1, 2, "3"]));
+console.log(without([-1, -2, -3], [-1]));
