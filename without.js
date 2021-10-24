867873c1d3be7390dba5cf4cd7a4502b1523c7fe
vagrant <wong_michael@rogers.com>
2021-10-24 03:34:35 +0000

    Revised without.js

--- without.js
+++ without.js
@@ -20,15 +20,20 @@ const assertArraysEqual = function(actual, expected) {
 };
 
 const without = function(sourceArray, itemToRemove) {
-let newSet = new Set(itemToRemove);
-let remove = [];
-for (let i = 0; i < sourceArray.length; i++) {
-  if (!newSet.has(sourceArray[i])) {
-    remove.push(sourceArray[i]);
+  let newSet = new Set(itemToRemove);
+  let remove = [];
+  for (let i = 0; i < sourceArray.length; i++) {
+    if (!newSet.has(sourceArray[i])) {
+      remove.push(sourceArray[i]);
+    }
   }
-}
-return remove;
+  return remove;
 };
 
-console.log(without([1, 2, 3], [1])) 
-console.log(without(["1", "2", "3"], [1, 2, "3"]))
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+
+console.log(without([1, 2, 3], [1]));
+console.log(without(["1", "2", "3"], [1, 2, "3"]));
