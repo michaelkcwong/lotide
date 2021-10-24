77f04a4e383f214ab86602aafacad2caa14b3763
vagrant <wong_michael@rogers.com>
2021-10-24 02:50:59 +0000

    added without.js

--- without.js
+++ without.js
@@ -1,10 +1,13 @@
 const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
       return false;
     }
-    return true;
   }
+  return true;
 };
 
 
@@ -15,3 +18,7 @@ const assertArraysEqual = function(arr1, arr2) {
     console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
   }
 };
+
+const without = function(sourceArr, itemsToRemove) {
+  
+}
