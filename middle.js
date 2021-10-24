376c31de87737f14ab2736828399092ebfdd8ad8
vagrant <wong_michael@rogers.com>
2021-10-24 20:40:12 +0000

    Changes in  middle.js

--- middle.js
+++ middle.js
@@ -21,8 +21,19 @@ const assertArraysEqual = function(actual, expected) {
 
 const middle = function(array) {
   let arrayMid = [];
-  if (array.length <= 2)
+  if (array.length <= 2) // if array length is <= 2 return empty array
   return [];
+  if (array.length %2 === 0) { // if array length is even return an array containing the two elements in the middle
+  return [array[Math.floor((array.length - 1) / 2)],array[array.length /2]];
+  } else {
+    return [array[Math.floor(array.length / 2)]]; //if array length is odd return an array containing single middle elemnent
+  }
 }
 
-console.log(middle([1,2]));
+
+console.log(middle([1]));
+console.log(middle([1, 2]));
+console.log(middle([1, 2 ,3]));
+console.log(middle([1, 2, 3, 4, 5]));
+console.log(middle([1, 2, 3, 4]));
+console.log(middle([1, 2, 3, 4, 5, 6]));
