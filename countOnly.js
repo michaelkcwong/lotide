39fc9901ce2c2833a05cc5ba7c52b02c23aefce0
vagrant <wong_michael@rogers.com>
2021-10-26 04:10:17 +0000

    Adding instructions to countOnly.js

--- countOnly.js
+++ countOnly.js
@@ -8,13 +8,17 @@ const assertEqual = function(actual, expected) {
 
 // allItems: an array of strings that we need to look through
 // itemsToCount: an object specifying what to count
-const countOnly = function(allItems, itemsToCount) {
+const countOnly = function(allItems, itemsToCount) { //Start by declaring the function.
   const results = {};
 
-  for (const item of allItems) {
-    if (itemsToCount[item]) { 
+  for (const item of allItems) { //Add code to loop over all the items in the array and print them to the console.
+// inside the loop, 
+// increment the counter for each item:
+//   set a property with that string key to:
+//     the value that was already there (or zero if nothing there) with 1 added to it.
+    if (itemsToCount[item]) { //Add an if condition to only increment our count in results if the item is found in the itemsToCount object.
       if (results[item]) {
-        results[item] += 1;
+        results[item] += 1; 
       } else {
         results[item] = 1;
       }
@@ -22,19 +26,19 @@ const countOnly = function(allItems, itemsToCount) {
   }
   console.log(results);
   return results;
-}
+};
 
-  const firstNames = [
-    "Karl",
-    "Salima",
-    "Agouhanna",
-    "Fang",
-    "Kavith",
-    "Jason",
-    "Salima",
-    "Fang",
-    "Joe"
-  ];
+const firstNames = [ //Write the initial test code below the definition of your function.
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
 
 const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
 
@@ -42,7 +46,7 @@ assertEqual(result1["Jason"], 1);
 assertEqual(result1["Karima"], undefined);
 assertEqual(result1["Fang"], 2);
 assertEqual(result1["Agouhanna"], undefined);
-
+//Confirm that all our tests are passing
 
 
 
