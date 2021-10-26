85ce1fc1a5e0e044f641deee984c71d798f566b1
vagrant <wong_michael@rogers.com>
2021-10-26 04:10:48 +0000

    Adding instructions to countOnly.js

--- countOnly.js
+++ countOnly.js
@@ -14,8 +14,8 @@ const countOnly = function(allItems, itemsToCount) { //Start by declaring the fu
   for (const item of allItems) { //Add code to loop over all the items in the array and print them to the console.
 // inside the loop, 
 // increment the counter for each item:
-//   set a property with that string key to:
-//     the value that was already there (or zero if nothing there) with 1 added to it.
+// set a property with that string key to:
+// the value that was already there (or zero if nothing there) with 1 added to it.
     if (itemsToCount[item]) { //Add an if condition to only increment our count in results if the item is found in the itemsToCount object.
       if (results[item]) {
         results[item] += 1; 
