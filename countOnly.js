9880f3f514e8eee075dba6b30489f34cffe731b6
vagrant <wong_michael@rogers.com>
2021-10-26 04:02:57 +0000

    Start countOnly.js

--- countOnly.js
+++ countOnly.js
@@ -9,6 +9,21 @@ const assertEqual = function(actual, expected) {
 // allItems: an array of strings that we need to look through
 // itemsToCount: an object specifying what to count
 const countOnly = function(allItems, itemsToCount) {
+  const results = {};
+
+  for (const item of allItems) {
+    if (itemsToCount[item]) { 
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+    }
+  }
+  console.log(results);
+  return results;
+}
+
   const firstNames = [
     "Karl",
     "Salima",
@@ -20,9 +35,8 @@ const countOnly = function(allItems, itemsToCount) {
     "Fang",
     "Joe"
   ];
-}
 
-const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false});
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
 
 assertEqual(result1["Jason"], 1);
 assertEqual(result1["Karima"], undefined);
