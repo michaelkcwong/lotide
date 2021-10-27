1b9ed2f013120c3af68b90680a4e5708136dd0f2
vagrant <wong_michael@rogers.com>
2021-10-27 05:31:50 +0000

    Finished letterPositions.js

--- letterPositions.js
+++ letterPositions.js
@@ -1,3 +1,6 @@
+//We'll implement a new function letterPositions which will return all the indices (zero-based positions) in the string where each character is found.
+//For each letter, instead of returning just one number to represent its number of occurrences, multiple numbers may be needed to represent all the places in the string that it shows up.
+
 const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
     return false;
@@ -21,8 +24,26 @@ const assertArraysEqual = function(actual, expected) {
 
 const letterPositions = function(sentence) {
   const results = {};
-
+  for (let i = 0; i < sentence.length; i++) {
+    const letter = sentence[i];
+    if (letter !== " ") {
+      if (results[letter]) {
+        results[letter].push(i);
+      } else {
+        results[letter] = [i];
+      }
+    }
+  }
   return results;
 };
 
+console.log(assertArraysEqual(letterPositions("hello").h, [0]));
 console.log(assertArraysEqual(letterPositions("hello").e, [1]));
+console.log(assertArraysEqual(letterPositions("hello").l, [2,3]));
+console.log(assertArraysEqual(letterPositions("hello").o, [4]));
+console.log(letterPositions("hello").h);
+console.log(letterPositions("hello").e);
+console.log(letterPositions("hello").l);
+console.log(letterPositions("hello").l);
+console.log(letterPositions("hello").o);
+console.log(letterPositions("hello"));
