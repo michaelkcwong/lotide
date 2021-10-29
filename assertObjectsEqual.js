ad8732f550c02511030500f9b18489a7797a340b
vagrant <wong_michael@rogers.com>
2021-10-29 05:36:48 +0000

    Finished map.js

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -16,12 +16,13 @@ const eqObjects = function(object1, object2) {
 };
 
 const assertObjectsEqual = function(actual, expected) {
-    if (eqObjects(actual, expected)) {
-      console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-    } else {
-      console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-    }
-  };
+  const inspect = require('util').inspect;
+  if (eqObjects(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+  }
+};
 
 
-  assertObjectsEqual({a: '1', b: '2'}, {b: '2', a: '1'});
+assertObjectsEqual({a: '1', b: '2'}, {b: '2', a: '1'});
