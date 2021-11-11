51b91053467eddf5fdb59aba62c49fd7a99682dc
vagrant <wong_michael@rogers.com>
2021-11-11 06:46:11 +0000

    Added mocha and chai to headTest.js

--- test/headTest.js
+++ test/headTest.js
@@ -2,9 +2,6 @@ const head = require('../head');
 const assertEqual = require('../assertEqual')
 const assert = require('chai').assert;
 
-// assertEqual(head([5,6,7]), 5);
-// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-
 describe("#head", () => {
   it("returns 1 for [1, 2, 3]", () => {
     assert.strictEqual(head([1, 2, 3]), 1);
@@ -18,3 +15,6 @@ describe("#head", () => {
     assert.strictEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hello');
 });
 });
+
+// assertEqual(head([5,6,7]), 5);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
