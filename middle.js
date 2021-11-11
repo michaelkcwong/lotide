9131f94a61ca0bcbb594464c82d1d61e0f1052d0
vagrant <wong_michael@rogers.com>
2021-11-11 05:57:13 +0000

    Refactored middle.js

--- middle.js
+++ middle.js
@@ -1,23 +1,6 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual');
 
-
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
 
 const middle = function(array) {
   if (array.length <= 2) // if array length is <= 2 return empty array
@@ -29,17 +12,4 @@ const middle = function(array) {
   }
 };
 
-
-console.log(assertArraysEqual(middle([1]), ([])));
-console.log(assertArraysEqual(middle([1, 2]), ([])));
-console.log(assertArraysEqual(middle([1, 2 ,3]), ([2])));
-console.log(assertArraysEqual(middle([1, 2, 3, 4, 5]), ([3])));
-console.log(assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]));
-console.log(assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]));
-
-console.log(middle([1]));
-console.log(middle([1, 2]));
-console.log(middle([1, 2, 3]));
-console.log(middle([1, 2, 3, 4, 5]));
-console.log(middle([1, 2, 3, 4]));
-console.log(middle([1, 2, 3, 4, 5, 6]));
+module.exports = middle;
