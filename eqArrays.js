73709059d58fd92845877773f0c16d57b3a41175
vagrant <wong_michael@rogers.com>
2021-11-11 05:47:52 +0000

    refactored eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
@@ -18,9 +12,6 @@ const eqArrays = function(array1, array2) {
   return true;
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([2, 1, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([2, 1, 3], [1, 2, 3]), false);
-assertEqual(eqArrays(["1", "2", 3], [1, 2, 3]), false);
-assertEqual(eqArrays([1, 0, 2], [5, 3, 1]), true);
+module.exports = eqArrays;
+
 
