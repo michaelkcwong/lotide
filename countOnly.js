6e7b3b7e45a61b5042ad9ba348874069412884e9
vagrant <wong_michael@rogers.com>
2021-11-20 21:18:31 +0000

    change

--- countOnly.js
+++ countOnly.js
@@ -1,18 +1,10 @@
 //A function that takes in a collection of items and returns a specific subset of those items.
 
-const assertEqual = require('./assertEqual')
-
-// allItems: an array of strings that we need to look through
-// itemsToCount: an object specifying what to count
-const countOnly = function(allItems, itemsToCount) { //Start by declaring the function.
+const countOnly = function(allItems, itemsToCount) { 
   const results = {};
 
-  for (const item of allItems) { //Add code to loop over all the items in the array and print them to the console.
-// inside the loop, 
-// increment the counter for each item:
-// set a property with that string key to:
-// the value that was already there (or zero if nothing there) with 1 added to it.
-    if (itemsToCount[item]) { //Add an if condition to only increment our count in results if the item is found in the itemsToCount object.
+  for (const item of allItems) { 
+    if (itemsToCount[item]) { 
       if (results[item]) {
         results[item] += 1; 
       } else {
