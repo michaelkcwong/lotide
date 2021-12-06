e6c2f6fea2b9ceab38a04a76954501918f3d1e52
vagrant <wong_michael@rogers.com>
2021-12-06 03:10:02 +0000

    Fix eqArrays to satisfy all test cases

--- eqArrays.js
+++ eqArrays.js
@@ -1,21 +1,26 @@
 //A function that takes in 2 arrays and returns true if the arrays are equal and false otherwise.
 
-const eqArrays = function(array1, array2) {
 
-  array1 = array1.flat();
-  array2 = array2.flat();
-
+const eqArrays = function (array1 , array2) {
   if (array1.length !== array2.length) {
-    return false;
+    return false; 
   }
 
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
+  for(let i = 0; i < array1.length; i++) {
+    if(Array.isArray(array1[i])) {
+      for (let j = 0; j < array1[i].length; j++) {
+        if(array1[i][j] !== array2[i][j]) {
+          return false;
+        }
+      }
+    }
+    else if (array1[i] !== array2[i]) {
       return false;
     }
   }
-  return true;
-};
+  
+  return true; 
+}
 
 module.exports = eqArrays;
 
--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -2,9 +2,12 @@ const eqArrays = require('../eqArrays');
 const assertEqual = require('../assertEqual');
 
 assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true);
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false);
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), true);
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 assertEqual(eqArrays([2, 1, 3], [1, 2, 3]), true);
 assertEqual(eqArrays([2, 1, 3], [1, 2, 3]), false);
 assertEqual(eqArrays(["1", "2", 3], [1, 2, 3]), false);
 assertEqual(eqArrays(["1", "2", 3], [1, 2, 3]), true);
 assertEqual(eqArrays([1, 0, 2], [5, 3, 1]), true);
+
